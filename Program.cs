5a0bdd528fa14f0f76e3a00e3bf060c5ea687eb0
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-04-23 20:56:48 -0700

    Added new gigignore file

--- Program.cs
+++ Program.cs
@@ -2,11 +2,10 @@
 
 namespace PhotoMosaics
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
--- obj/Debug/net5.0/PhotoMosaics.AssemblyInfo.cs
+++ obj/Debug/net5.0/PhotoMosaics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoMosaics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431ccb185fe0c6633d41aa1bc57771b9a1b33dc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoMosaics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoMosaics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
